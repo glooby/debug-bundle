998836d7e6a5bc622b7da8537584b416be529721
Emil Kilhage <emilkilhage@gmail.com>
2015-11-16 21:17:50 +0100

    \Exception::__toString is already including the previous ones
    Signed-off-by: Emil Kilhage <emilkilhage@gmail.com>

--- src/Debug/Formatter/ExceptionFormatter.php
+++ src/Debug/Formatter/ExceptionFormatter.php
@@ -15,23 +15,6 @@ class ExceptionFormatter implements FormatterInterface
     {
         $text = "$exception\n";
 
-        $text = $this->addPrevious($exception, $text);
-
-        return $text;
-    }
-
-    /**
-     * @param \Exception $exception
-     * @param string     $text
-     * @return string
-     */
-    private function addPrevious($exception, $text)
-    {
-        if ($exception->getPrevious() instanceof \Exception) {
-            $text .= "\nPrevious:\n----------\n" . $this->format($exception->getPrevious());
-            return $text;
-        }
-
         return $text;
     }
 }
